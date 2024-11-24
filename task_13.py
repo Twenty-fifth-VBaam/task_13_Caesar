833ce6a15b5e43c992886bd3878803e4839c5d53
Pavel <Pavel@nomail.ru>
2024-11-24 10:05:37 +0300

    added encrypt method

--- task_13.py
+++ task_13.py
@@ -2,22 +2,23 @@
 
 
 class CaesarsCipher:
-    Code_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
-    En_dict = enchant.Dict('en_US')
-    @staticmethod
-    def decrypt(info):
-        for key in range(len(CaesarsCipher.Code_chars)):
+    def __init__(self):
+        self.code_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+        self.En_dict = enchant.Dict('en_US')
+
+    def decrypt(self, info):
+        for key in range(len(self.code_chars)):
             result = ''
 
             for curr_chr in info:
-                index = CaesarsCipher.Code_chars.find(curr_chr)
-                if index + key >= len(CaesarsCipher.Code_chars):
-                    index -= len(CaesarsCipher.Code_chars)
-                result += CaesarsCipher.Code_chars[index + key]
+                index = self.code_chars.find(curr_chr)
+                if index + key >= len(self.code_chars):
+                    index -= len(self.code_chars)
+                result += self.code_chars[index + key]
 
             flag_check_info = False
             for i in result.split():
-                if CaesarsCipher.En_dict.check(i) == False:
+                if self.En_dict.check(i) == False:
                     flag_check_info = False
                     break
                 else:
@@ -25,18 +26,20 @@ class CaesarsCipher:
             if flag_check_info == True:
                 return key, result
 
-            # if (CaesarsCipher.En_dict.check(result.split()[0]) and
-            #         CaesarsCipher.En_dict.check(result.split()[1])):
-            #     return key, result
-
-    @staticmethod
-    def encrypt(info):
-        pass
+    def encrypt(self, info, key):
+        result = ''
+        for curr_char in info:
+            index = self.code_chars.find(curr_char)
+            if index + key >= len(self.code_chars):
+                index -= len(self.code_chars)
+            result += self.code_chars[index + key]
+        return result
 
 
 if __name__ == '__main__':
     string_to_be_checked = 'o3zR v..D0?yRA0R8FR8v47w0ER4.R1WdC!sLF5D'
     test_str = 'Wkh.ydfdwlrq.zdv.d.vxffhvv'
-
-    print(f'key: {CaesarsCipher.decrypt(test_str)[0]}, '
-          f'result: {CaesarsCipher.decrypt(test_str)[1]}')
+    cipher = CaesarsCipher()
+    print(f'key: {cipher.decrypt(test_str)[0]}, '
+          f'result: {cipher.decrypt(test_str)[1]}')
+    print(cipher.encrypt(cipher.decrypt(test_str)[1], -cipher.decrypt(test_str)[0]))
